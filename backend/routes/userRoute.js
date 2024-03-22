21244feba3d29aaa5bda100b9ebaa789aa0b1573
YatharthDedhia <dedhiayatharth2004@gmail.com>
2024-03-22 23:52:17 +0530

    added backedn

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const userController = require('../controllers/userController')
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+const router = express.Router();
+
+router.route("/register")
+    .post(userController.registerUser);
+
+router.route("/login")
+    .post(userController.loginUser);
+
+router.route("/password/forgot")
+    .post(userController.forgotPassword);
+
+router.route("/password/:token")
+    .put(userController.resetPassword);
+
+router.route("/logout")
+    .get(userController.logOut);
+
+router.route("/password/update")
+    .put(isAuthenticatedUser, userController.updatePassword);
+
+router.route("/me/update")
+    .put(isAuthenticatedUser, userController.updateProfile);
+
+router.route("/me")
+    .get(isAuthenticatedUser, userController.getUserDetails);
+
+router.route("/admin/users")
+    .get(isAuthenticatedUser, authorizeRoles("admin"), userController.getAllUser);
+
+router.route("/admin/user/:id")
+    .get(isAuthenticatedUser, authorizeRoles("admin"), userController.getUserbyType)
+    // .put(isAuthenticatedUser, authorizeRoles("admin"), userController.updateUserRole)
+    // .delete(isAuthenticatedUser, authorizeRoles("admin"), userController.deleteUser);
+
+router.route("/lawyerlist/:id")
+    .get(isAuthenticatedUser, userController.getUserbyType)
+
+
+    
+
+module.exports = router;
