dc9551bb41604fa1218525da6441d10297b10c78
JB-Shah <jenils2003@gmail.com>
2024-03-22 20:06:37 +0530

    Testing Model

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,13 +9,13 @@ import "flowbite";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      {/* <div className="tagline mt-10">
+      {/* <Navbar />
+      <div className="tagline mt-10">
         <p className="text-5xl mx-10 font-bold text-wrap text-cyan-500 text-center">Meet your new AI legal assistant </p> 
         <p className="text-gray-400 font-xl text-center mt-3 w-1/2 mx-auto">CoCounsel does document review,deposition preparation, contract analysis, and timeline creation in
         minutes—with results you can trust.</p>
-      </div>
-      <Input/>  */}
+      </div> */}
+      <Input/> 
       <FIR />
     </div>
   );
