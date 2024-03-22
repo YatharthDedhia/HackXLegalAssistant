2a3fc8f3a3c30241d323ab166a6bab3ad42e6613
YatharthDedhia <dedhiayatharth2004@gmail.com>
2024-03-22 19:32:16 +0530

    added summarizer and citations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import GMaps from './components/GMaps';
+import Leaflet from './components/LeafLet';
 import Navbar from './components/Navbar';
 import "flowbite"
+import Summarise from './components/Summarise';
+import Citations from './components/Citations';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      {/* <GMaps /> */}
+      {/* <Leaflet /> */}
+      {/* <Summarise /> */}
+      <Citations />
     </div>
   );
 }
