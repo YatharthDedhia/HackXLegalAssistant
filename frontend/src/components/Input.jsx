49e744d7f37a377983a7efe87708804b8f662a42
JB-Shah <jenils2003@gmail.com>
2024-03-23 05:22:41 +0530

    fixing errors

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -114,58 +114,6 @@ const Input = () => {
 
 
   return (
-<<<<<<< HEAD
-    <div className="input-field mt-10 sm:w-full sm:px-10">
-      <div className="tagline mt-10">
-        <p className="text-5xl mx-10 font-bold text-wrap text-cyan-500 text-center">
-          Meet your new AI legal assistant{" "}
-        </p>
-        <p className="text-gray-400 font-xl text-center mt-3 w-1/2 mx-auto">
-          CoCounsel does document review,deposition preparation, contract
-          analysis, and timeline creation in minutes—with results you can trust.
-        </p>
-      </div>
-      <div class="mb-6 sm:flex sm:flex-row sm:w-full flex flex-col gap-10 sm:mx-auto mt-10 ">
-        <textarea
-          type="text"
-          id="default-input"
-          class="bg-gray-300 text-gray-900 text-md rounded-xl sm:w-5/12 w-full h-md border border-10 border-blue-500 dark:bg-gray-700 dark:text-white dark:border-blue-500 shadow-xl"
-          placeholder="Enter Case Details..."
-          value={caseInput}
-          onChange={(e) => setCaseInput(e.target.value)}
-        ></textarea>
-        <div class="output-field bg-gray-300 border border-10 border-blue-500 text-gray-900 text-md rounded-xl sm:w-5/12 w-full block p-3 dark:bg-gray-700 dark:text-white dark:border-blue-500 overflow-y-auto h-56 text-left">
-          <ReactMarkdown>{apiData}</ReactMarkdown>
-        </div>
-      </div>
-      <button
-        class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
-        onClick={(e) => {
-          e.preventDefault(); // Prevent default form submission behavior
-          geminiSummarize(caseInput); // Call callGemini with caseInput when form is submitted
-        }}
-      >
-        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-          Summarize
-        </span>
-      </button>
-      <button
-        class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
-        onClick={(e) => {
-          e.preventDefault(); // Prevent default form submission behavior
-          geminiCitations(caseInput); // Call callGemini with caseInput when form is submitted
-        }}
-      >
-        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-          Citation
-        </span>
-      </button>
-      <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-          Similar Case
-        </span>
-      </button>
-=======
     <div className="contianer flex justify-center items-center h-screen ">
       <div className="input-field  sm:w-full sm:px-10 mb-10 ">
         <div className="tagline sm:mt-20 mt-20 ">
@@ -251,7 +199,6 @@ const Input = () => {
 
   
       </div>
->>>>>>> 624668226d9879b7bf6a8914f835744a4b65d69f
     </div>
   );
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,17 +21,10 @@ const Navbar = () => {
                             <a href="/template" class="block text-2xl py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Template</a>
                         </li>
                         <li>
-<<<<<<< HEAD
-                            <a href="/findlawyer" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Find Lawyer</a>
-                        </li>
-                        <li>
-                            <a href="/register" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Register Lawyer</a>
-=======
                             <a href="#" class="block py-2 px-3 text-2xl text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
                         </li>
                         <li>
                             <a href="#" class="block py-2 px-3 text-2xl text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
->>>>>>> 624668226d9879b7bf6a8914f835744a4b65d69f
                         </li>
                         <li>
                             <a href="#" class="block py-2 px-3 text-2xl text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
